e26febda8c1177cad5cb42247c7b6d90f64d67ac
SharpieGod <sabodarko09@gmail.com>
2026-04-18 20:51:27 -0700

    maybe update

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React, { useActionState } from "react";
 import DarkoButton from "./Darko/DarkoButton";
-import { P } from "node_modules/framer-motion/dist/types.d-B50aGbjN";
 import Link from "next/link";
 
 const LoginForm = ({
@@ -18,15 +17,15 @@ const LoginForm = ({
     <div>
       <form
         action={formAction}
-        className="border-text-800 m-10 mx-auto flex w-2/5 flex-col gap-8 rounded-lg border p-8"
+        className="m-10 mx-auto flex w-2/5 flex-col gap-8 rounded-lg border border-text-800 p-8"
       >
-        <h1 className="text-background-200 text-start text-3xl">Login</h1>
+        <h1 className="text-start text-3xl text-background-200">Login</h1>
         <div className="flex flex-col gap-4">
           <span className="text-lg text-red-400">{error}</span>
           <div className="flex flex-col gap-1">
             <span>Email</span>
             <input
-              className="focus:outline-accent-500 bg-background-900 rounded-lg bg-opacity-50 p-3 focus:outline"
+              className="rounded-lg bg-background-900 bg-opacity-50 p-3 focus:outline focus:outline-accent-500"
               type="email"
               name="email"
               placeholder="Email"
@@ -36,7 +35,7 @@ const LoginForm = ({
           <div className="flex flex-col gap-1">
             <span>Password</span>
             <input
-              className="focus:outline-accent-500 bg-background-900 rounded-lg bg-opacity-50 p-3 focus:outline"
+              className="rounded-lg bg-background-900 bg-opacity-50 p-3 focus:outline focus:outline-accent-500"
               type="password"
               name="password"
               placeholder="Password"
